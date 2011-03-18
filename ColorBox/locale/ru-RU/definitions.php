cad9d600b8fbe75921657e039c83e9e7a84c56c3
Ronnie <unlights@gmail.com>
2011-03-19 00:07:58 +0300

    Moved to directory

---
+++ ColorBox/locale/ru-RU/definitions.php
@@ -0,0 +1,7 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$Definition['colorbox-current'] = '{current} / {total}';
+$Definition['colorbox-next'] = 'след.';
+$Definition['colorbox-previous'] = 'пред.';
+$Definition['colorbox-close'] = 'закрыть';
+
