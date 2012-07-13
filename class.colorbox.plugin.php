a11b5e5d3a04cc5c732429128fb384f10c4b2a43
iamthes <s91630277@gmail.com>
2012-07-14 03:36:16 +0400

    Update to v1.3.19.3

--- class.colorbox.plugin.php
+++ class.colorbox.plugin.php
@@ -3,23 +3,23 @@
 $PluginInfo['ColorBox'] = array(
 	'Name' => 'ColorBox',
 	'Description' => 'A light-weight, customizable lightbox plugin for jQuery.',
-	'Version' => '1.0.3',
-	'Date' => '15 Feb 2011',
+	'Version' => '1.1.3.19.3',
+	'Date' => '13 Apr 2011',
 	'Author' => 'Jack Moore',
 	'AuthorUrl' => 'http://colorpowered.com/colorbox/'
 );
-
+v
 class ColorBoxPlugin extends Gdn_Plugin {
 	
 	static $Initialized = False;
 	
-	static public function Initialize(&$Controller, $CssFile = 4) {
+	static public function Initialize(&$Controller, $CssFile = 4, $ApplicationFolder = '') {
 		if (!self::$Initialized) {
 			self::$Initialized = True;
 			if (is_numeric($CssFile)) {
 				$Controller->AddCssFile('plugins/ColorBox/design/colorbox'.$CssFile.'.css');
-				$Controller->AddCssFile('custom-colorbox'.$CssFile.'.css');
-			} else $Controller->AddCssFile($CssFile);
+				$Controller->AddCssFile('custom-colorbox'.$CssFile.'.css', $ApplicationFolder);
+			} else $Controller->AddCssFile($CssFile, $ApplicationFolder);
 			
 			$Controller->AddJsFile('plugins/ColorBox/js/jquery.colorbox.js');
 			
